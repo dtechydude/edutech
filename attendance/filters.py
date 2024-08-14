c455f13ac484a2d3edbc27f09fc038f9aa9d78b6
“Gbenga <dtechydude@gmail.com>
2024-08-14 20:53:18 +0100

    attendance module update

--- attendance/filters.py
+++ attendance/filters.py
@@ -1,11 +1,11 @@
-import django_filters
-from .models import Attendance
-from django.contrib.auth.models import User
+# import django_filters
+# from .models import Attendance
+# from django.contrib.auth.models import User
 
-class AttendanceFilter(django_filters.FilterSet):
+# class AttendanceFilter(django_filters.FilterSet):
 
-    class Meta:
-        model = Attendance
+#     class Meta:
+#         model = Attendance
   
-        fields = {'student_id', 'student_id__current_class',}
+#         fields = {'student_id', 'student_id__current_class',}
         
--- attendance/forms.py
+++ attendance/forms.py
@@ -1,45 +1,44 @@
-from django import forms
-from .models import Attendance
-from students.models import StudentDetail
+# from django import forms
+# from attendance.models import Attendance
+# from students.models import StudentDetail
 
 
-
-class StudentAttendanceForm(forms.ModelForm):
+# # class StudentAttendanceForm(forms.ModelForm):
     
-    class Meta:
-        model = Attendance
-        fields = ['student_id', 'session', 'attendance_date', 'morning_status', 'afternoon_status', 'authorized_sign']
-        widgets = {
-            'attendance_date': forms.DateInput(
-                format=('%d/%m/%Y'),
-                attrs={'class': 'form-control', 
-                       'placeholder': 'Select a date',
-                       'type': 'date'  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
-                      }),
-        }
+# #     class Meta:
+# #         model = Attendance
+# #         fields = ['student_id', 'session', 'attendance_date', 'morning_status', 'afternoon_status', 'authorized_sign']
+# #         widgets = {
+# #             'attendance_date': forms.DateInput(
+# #                 format=('%d/%m/%Y'),
+# #                 attrs={'class': 'form-control', 
+# #                        'placeholder': 'Select a date',
+# #                        'type': 'date'  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
+# #                       }),
+# #         }
 
-class MyStudentAttendanceForm(forms.ModelForm):
+# # class MyStudentAttendanceForm(forms.ModelForm):
     
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['student_id'].queryset = StudentDetail.objects.filter(class_teacher__user=request.user).order_by('student_username')
-    #     # self.fields['student_id'].queryset = Attendance.objects.filter(student_id__class_teacher__user=request.user)
+# #     # def __init__(self, *args, **kwargs):
+# #     #     super().__init__(*args, **kwargs)
+# #     #     self.fields['student_id'].queryset = StudentDetail.objects.filter(class_teacher__user=request.user).order_by('student_username')
+# #     #     # self.fields['student_id'].queryset = Attendance.objects.filter(student_id__class_teacher__user=request.user)
     
-    class Meta:
-        model = Attendance
-        def __init__(self, *args, **kwargs):
-            student_id = kwargs.pop('student_id', None)
-            super(MyStudentAttendanceForm, self).__init__(*args, **kwargs)
+# #     class Meta:
+# #         model = Attendance
+# #         def __init__(self, *args, **kwargs):
+# #             student_id = kwargs.pop('student_id', None)
+# #             super(MyStudentAttendanceForm, self).__init__(*args, **kwargs)
         
-        fields = '__all__'
-        widgets = {
-            'attendance_date': forms.DateInput(
-                format=('%d/%m/%Y'),
-                attrs={'class': 'form-control', 
-                       'placeholder': 'Select a date',
-                       'type': 'date'  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
-                      }),
-        }
+# #         fields = '__all__'
+# #         widgets = {
+# #             'attendance_date': forms.DateInput(
+# #                 format=('%d/%m/%Y'),
+# #                 attrs={'class': 'form-control', 
+# #                        'placeholder': 'Select a date',
+# #                        'type': 'date'  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
+# #                       }),
+# #         }
 
 
 
